835c4aae8d116e7217473ba6ab02996004433f48
DougFair <doug.fairlie@onjcri.org.au>
2020-01-09 07:49:50 +1100

    fix contact page

--- src/Contact.js
+++ src/Contact.js
@@ -7,7 +7,7 @@ const Contact = () =>
                 <div className="contactContainer">
                     <div className="contactHeader">
                             <h1>Contact Us!</h1>
-                            <p> You can contact us at <span>contact@mripubs.com</span>. Please feel free to provide any feedback on any aspect of this site. We are very open to suggestions! 
+                            <p> You can contact us at <span>contact@mripubs.com</span>. Please feel free to provide feedback on any aspect of this site. We are very open to suggestions! 
                             </p>
                     </div>
                 </div>
