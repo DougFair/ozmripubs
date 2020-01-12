4df033d914d56eec0a2cf31ba2ac4c959debc25d
DougFair <doug.fairlie@onjcri.org.au>
2020-01-12 14:02:59 +1100

    fix subheading typo

--- src/SubHeading.js
+++ src/SubHeading.js
@@ -5,7 +5,7 @@ const SubHeading = () => {
     <div className="subBanner" style={{margin: "0 20px"
 }}>
         <p className="subBannerText" style={{lineHeight: 1.3, fontSize: "0.9rem"
-        }}>If you work at an Australian Medical Research Institute, or are just interested in what your colleagues at MRIs are doing, <span style={{fontWeight: "bold", fontStyle: "italic", color: "orange" }}>"Aussie MRI Publications"</span>  &nbsp;is an easy way to view their latest papers. If you want to know what biomed paper are being published at Aussie universities instead, go to our sister site <span style={{fontWeight: "bold", fontStyle: "italic", color: "purple" }}>"Aussie Uni Publications"</span></p>
+        }}>If you work at an Australian Medical Research Institute, or are just interested in what your colleagues at MRIs are doing, <span style={{fontWeight: "bold", fontStyle: "italic", color: "orange" }}>"Aussie MRI Publications"</span>  &nbsp;is an easy way to view their latest papers. If you want to know what biomed papers are being published at Aussie universities instead, go to our sister site <span style={{fontWeight: "bold", fontStyle: "italic", color: "purple" }}>"Aussie Uni Publications"</span></p>
         <hr style={{marginTop: "20px"}}/>
     </div>
     )
